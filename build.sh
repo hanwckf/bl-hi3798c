75f69451bbd64a6aea25c4068ca8786f6e5aa310
hanwckf <my375229675@gmail.com>
2020-04-15 12:12:39 +0800

    build.sh: build atf with release varient

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 UBOOT_CFG=u-boot-sn1.config
+ATF_DEBUG=0
 CROSS_64=aarch64-linux-gnu-
 CROSS_32=arm-linux-gnueabihf-
 
@@ -22,25 +23,31 @@ if [ ! -f u-boot/u-boot.bin ]; then
 fi
 
 echo "######  BUILD ATF ######"
+if [ $ATF_DEBUG = 0 ]; then
+	ATF_BIN_DIR=release
+else
+	ATF_BIN_DIR=debug
+fi
+
 make -C atf distclean
 if [ "$1" = "RECOVERY" ]; then
 	echo "BUILD ATF WITH RECOVERY"
 	make -C atf CROSS_COMPILE=${CROSS_64} all fip \
-		DEBUG=1 PLAT=poplar SPD=none BL33=../u-boot/u-boot.bin \
+		DEBUG=${ATF_DEBUG} PLAT=poplar SPD=none BL33=../u-boot/u-boot.bin \
 		POPLAR_RECOVERY=1
 else
 	make -C atf CROSS_COMPILE=${CROSS_64} all fip \
-		DEBUG=1 PLAT=poplar SPD=none BL33=../u-boot/u-boot.bin
+		DEBUG=${ATF_DEBUG} PLAT=poplar SPD=none BL33=../u-boot/u-boot.bin
 fi
 
-if [ ! -f ./atf/build/poplar/debug/bl1.bin ] || [ ! -f ./atf/build/poplar/debug/fip.bin ]; then
+if [ ! -f ./atf/build/poplar/${ATF_BIN_DIR}/bl1.bin ] || [ ! -f ./atf/build/poplar/${ATF_BIN_DIR}/fip.bin ]; then
 	echo "ATF build failed!"
 	exit 1
 fi
 
 echo "######  BUILD l-loader ######"
-cp -f ./atf/build/poplar/debug/bl1.bin ./l-loader/atf/
-cp -f ./atf/build/poplar/debug/fip.bin ./l-loader/atf/
+cp -f ./atf/build/poplar/${ATF_BIN_DIR}/bl1.bin ./l-loader/atf/
+cp -f ./atf/build/poplar/${ATF_BIN_DIR}/fip.bin ./l-loader/atf/
 make -C l-loader clean
 if [ "$1" = "RECOVERY" ]; then
 	echo "BUILD l-loader WITH RECOVERY"
